04d253970588934bdb2a84bda4bc8067b6c99bbd
Anton <it2015maistrenko@gmail.com>
2019-07-25 18:30:47 +0300

    Blog test, rename test files.

---
+++ tests/unit/test_blog.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from blog import Blog
+
+
+class BlogTest(TestCase):
+    def test_create_blog(self):
+        b = Blog('Test', 'Test Author')
+
+        self.assertEqual(b.title, 'Test')
+        self.assertEqual(b.author, 'Test Author')
+        self.assertListEqual(b.posts, [])
+        self.assertEqual(len(b.posts), 0)
