5fde45ef1959a54d1e444738d6d1d7aeb3200204
Anton <it2015maistrenko@gmail.com>
2019-07-25 18:57:40 +0300

    the __repr__ method, TDD

--- blog.py
+++ blog.py
@@ -3,3 +3,12 @@ class Blog:
         self.title = title
         self.author = author
         self.posts = []
+
+    def __repr__(self):
+        return f'{self.title} by {self.author} ({len(self.posts)} post{"" if len(self.posts) == 1 else "s"})'
+
+    def create_post(self):
+        pass
+
+    def json(self):
+        pass
--- tests/unit/test_blog.py
+++ tests/unit/test_blog.py
@@ -11,3 +11,19 @@ class BlogTest(TestCase):
         self.assertEqual(b.author, 'Test Author')
         self.assertListEqual(b.posts, [])
         self.assertEqual(len(b.posts), 0)
+
+    def test_repr(self):
+        b1 = Blog('Test', 'Test Author')
+        b2 = Blog('The best book', 'John Doe')
+        self.assertEqual(b1.__repr__(), 'Test by Test Author (0 posts)')
+        self.assertEqual(b2.__repr__(), 'The best book by John Doe (0 posts)')
+
+    def test_repr_multiple_posts(self):
+        b1 = Blog('The best book', 'John Doe')
+        b1.posts = ['first', 'second', 'third']
+        self.assertEqual(b1.__repr__(), 'The best book by John Doe (3 posts)')
+
+    def test_repr_single_post(self):
+        b1 = Blog('The best book', 'John Doe')
+        b1.posts = ['first']
+        self.assertEqual(b1.__repr__(), 'The best book by John Doe (1 post)')
