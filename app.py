741496e4bd5036e8b89585cb84ebc742691f6ca3
Anton <it2015maistrenko@gmail.com>
2019-07-26 19:03:08 +0300

    TestCase SetUp.

--- app.py
+++ app.py
@@ -1,14 +1,58 @@
-MENU_PROMPT = 'Enter "c" to create  blog, "l" to list the blogs, "r" to read one, "p" to create a post, or "q" to quit:'
+from blog import Blog
+from post import Post
 
+MENU_PROMPT = 'Enter "c" to create  blog, "l" to list the blogs, "r" to read one, "p" to create a post, or "q" to quit:'
+POST_TEMPLATE = '''
+--- {} ---
+
+{}
+    
+'''
 blogs = dict()
 
 
 def menu():
-    selection = input(MENU_PROMPT)
     print_blogs()
+    selection = input(MENU_PROMPT)
+    while selection != 'q':
+        if selection == 'c':
+            ask_create_blog()
+        elif selection == '1':
+            print_blogs()
+        elif selection == 'r':
+            ask_to_read_blog()
+        elif selection == 'p':
+            ask_create_post()
+        selection = input(MENU_PROMPT)
 
 
 def print_blogs():
     for key, blog in blogs.items():
         print('- {}'.format(blog))
 
+
+def ask_create_blog():
+    title = input('Enter your blog title: ')
+    author = input('Enter your name: ')
+    blogs[title] = Blog(title, author)
+
+
+def ask_to_read_blog():
+    title = input('Enter the blog you want to read:')
+    print_posts(blogs.get(title))
+
+
+def print_posts(blog):
+    for post in blog.posts:
+        print_post(post)
+
+
+def print_post(post):
+    print(POST_TEMPLATE.format(post.title, post.content))
+
+
+def ask_create_post():
+    blog_name = input('Enter your post title: ')
+    title = input('Enter your post title: ')
+    content = input('Enter the content: ')
+    blogs[blog_name].create_post(title, content)
--- tests/system/test_app.py
+++ tests/system/test_app.py
@@ -2,23 +2,53 @@ import app
 from unittest import TestCase
 from unittest.mock import patch
 from blog import Blog
+from post import Post
 
 
 class AppTest(TestCase):
-    def test_menu_input_prompt(self):
-        with patch('builtins.input') as mocked_input:
+
+    def setUp(self) -> None:
+        self.blog = Blog('Test', 'Test Author')
+        app.blogs = {'Test': self.blog}
+
+    def test_menu_calls_create_blog(self):
+        with patch('builtins.input') as input_mock:
+            input_mock.side_effect = ('c', 'Test Create Blog', 'Test Author', 'q')
             app.menu()
-            mocked_input.assert_called_with(app.MENU_PROMPT)
+            self.assertIsNotNone(app.blogs['Test Create Blog'])
+
+    def test_menu_input_prompt(self):
+        with patch('builtins.input', return_value='q') as input_mock:
+            app.menu()
+            input_mock.assert_called_with(app.MENU_PROMPT)
 
     def test_menu_calls_print_blog(self):
         with patch('app.print_blogs') as mocked_print_blogs:
-            with patch('builtins.input'):
+            with patch('builtins.input', return_value='q'):
                 app.menu()
                 mocked_print_blogs.assert_called()
 
     def test_print_blogs(self):
-        blog = Blog('Test', 'Test Author')
-        app.blogs = {'Test': blog}
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
             mocked_print.assert_called_with('- Test by Test Author (0 posts)')
+
+    def test_ask_create_blog(self):
+        with patch('builtins.input') as input_mock:
+            input_mock.side_effect = ('Test', 'Test Author')
+            app.ask_create_blog()
+            self.assertEqual(str(app.blogs['Test']), 'Test by Test Author (0 posts)')
+
+    def test_ask_to_read_blog(self):
+        self.blog.posts.append(Post('Post', 'Content'))
+        with patch('builtins.input', return_value='Test'):
+            with patch('builtins.print') as print_mock:
+                app.ask_to_read_blog()
+                print_mock.assert_called_with('\n--- Post ---\n\nContent\n    \n')
+
+    def test_ask_create_post(self):
+        with patch('builtins.input') as input_mock:
+            input_mock.side_effect = ('Test', 'Test post title', 'Test content')
+            app.ask_create_post()
+            self.assertEqual(app.blogs['Test'].posts[0].title, 'Test post title')
+            self.assertEqual(app.blogs['Test'].posts[0].content, 'Test content')
