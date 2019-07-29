88d8137a1209c7f1398e78f19bdd2ece197983b5
Anton <it2015maistrenko@gmail.com>
2019-07-29 19:18:23 +0300

    User tests. Les 64.

--- starter_code2/app.py
+++ starter_code2/app.py
@@ -1,10 +1,13 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
+from flask_jwt import JWT, JWTError
 
+from security import authenticate, identity
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from resources.user import UserRegister
 
 app = Flask(__name__)
 
@@ -12,13 +15,21 @@ app.config['DEBUG'] = True
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'jo9999'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
+api.add_resource(UserRegister, '/register')
+
+
+@app.errorhandler(JWTError)
+def auth_error_handler(err):
+    return jsonify({'message': 'Could not authorize'}), 401
 
 
 if __name__ == '__main__':
